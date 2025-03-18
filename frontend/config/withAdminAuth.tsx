08d1479e0a2e0d0e8a8585fd765b468c3c88734c
hupptechnologies <git@hupp.in>
2025-03-18 14:59:16 +0530

    [Add] - Admin auth hook

---
+++ frontend/config/withAdminAuth.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { appLocalStorage } from '@/utils/helper';
+
+const withAdminAuth = <P extends object>(Component: React.ComponentType<P>) => {
+	return function ProtectedRoute (props: P) {
+		const router = useRouter();
+		const isAdmin = appLocalStorage.getItem('isAdmin');
+
+		useEffect(() => {
+			if (!isAdmin) {
+				router.push('/login');
+			}
+		}, [isAdmin, router]);
+
+		return isAdmin ? <Component {...props} /> : null;
+	};
+};
+
+export default withAdminAuth;
